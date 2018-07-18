bd96a52b860c2f190cbf682fc54041dd4a70c575
Joris Hoogeboom <jorishoogeboom@gmail.com>
2018-07-18 10:53:28 +0200

    temp disable dev for testing on mac

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # /etc/init.d/RepetierServer start
-# service RepetierServer start
+rm /etc/init.d/RepetierServer
+chmod 644 /lib/systemd/system/RepetierServer.service
+systemctl --system daemon-reload
+systemctl enable RepetierServer.service
+service RepetierServer start
 
-/bin/mkdir -p /var/lib/Repetier-Server
-/bin/chown -R repetierserver /var/lib/Repetier-Server
-/usr/local/Repetier-Server/bin/RepetierServer -c /usr/local/Repetier-Server/etc/RepetierServer.xml --daemon
+# /bin/mkdir -p /var/lib/Repetier-Server
+# /bin/chown -R repetierserver /var/lib/Repetier-Server
+# /usr/local/Repetier-Server/bin/RepetierServer -c /usr/local/Repetier-Server/etc/RepetierServer.xml --daemon
 
 exec "$@"
